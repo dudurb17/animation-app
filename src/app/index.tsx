3bd20dfce15553fdd0c34a28fea853196c0ea615
Eduardo <eduardo.bedin@olli.vc>
2025-08-04 17:08:07 -0300

    Refactor animation in Index component to simplify logic by removing looping and adjusting width and height interpolation. Cleaned up unused imports and components.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,33 @@
 import { useEffect, useRef } from "react";
-import { Animated, Text, View } from "react-native";
+import { Animated, View } from "react-native";
 
 export default function Index() {
   const larAnimada = useRef(new Animated.Value(100)).current;
   const altAnimada = useRef(new Animated.Value(100)).current;
 
+  let porcentaje = larAnimada.interpolate({
+    inputRange: [0, 300],
+    outputRange: ["0%", "100%"],
+  });
+
+  let porcentajeAlt = altAnimada.interpolate({
+    inputRange: [0, 300],
+    outputRange: ["0%", "100%"],
+  });
+
   const animar = () => {
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(larAnimada, {
-          toValue: 300,
-          duration: 2000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(larAnimada, {
-          toValue: 100,
-          duration: 2000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(altAnimada, {
-          toValue: 300,
-          duration: 2000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(altAnimada, {
-          toValue: 100,
-          duration: 2000,
-          useNativeDriver: false,
-        }),
-      ])
-    ).start();
+    Animated.sequence([
+      Animated.timing(larAnimada, {
+        toValue: 300,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(altAnimada, {
+        toValue: 300,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+    ]).start();
   };
 
   useEffect(() => {
@@ -46,16 +44,14 @@ export default function Index() {
     >
       <Animated.View
         style={{
-          width: larAnimada,
-          height: altAnimada,
+          width: porcentaje,
+          height: porcentajeAlt,
           backgroundColor: "red",
           alignItems: "center",
           justifyContent: "center",
           borderRadius: 10,
         }}
-      >
-        <Text style={{ color: "white" }}>Hello World!!</Text>
-      </Animated.View>
+      />
     </View>
   );
 }
